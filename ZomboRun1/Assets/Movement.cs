7b725cb33d4dab386c86146211fe6aa5f461b88f
(Student) - David Scott <c00208031@ITCARLOW.IE>
2017-04-06 14:27:54 +0100

    Google VR game with Unity

---
+++ ZomboRun1/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+
+    // public GameObject ground;
+    private Vector3 spawnPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        spawnPoint = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.acceleration.x, 0);
+
+        transform.position = transform.position + Camera.main.transform.forward * 3.5f * Time.deltaTime;
+        
+        if (transform.position.y < -10)
+        {
+            transform.position = spawnPoint;
+        }
+
+    }
+
+}
